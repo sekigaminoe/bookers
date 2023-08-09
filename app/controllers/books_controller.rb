fbc1481266e92c6816af013d3fa5440233c2e0cd
EC2 Default User <ec2-user@ip-172-31-16-124.ap-northeast-1.compute.internal>
2023-08-09 09:31:53 +0000

    [Finish] バリデーション機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,16 +24,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated"
-    redirect_to book_path(book.id)
+      @book = Book.find(params[:id])
+      if @book.update(book_params)
+         flash[:notice] = "Book was successfully updated."
+         redirect_to book_path(@book)
+      else
+         render :edit
+      end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path(@book)
   end
 
   private
